ae9dd19589d67f1aa672f4274173408a0e0e99e3
waltersegovia <wseg@hotmail.com>
2020-06-30 05:13:11 -0300

    Add files via upload

---
+++ TP-04/UnitTestTP4/UnitTestEntidades.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace UnitTestTP4
+{
+    [TestClass]
+    public class UnitTestEntidades
+    {
+        /// <summary>
+        /// Verifiqua que no se puedan cargar dos Paquetes con el mismo Tracking ID.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void PaqueteRepetido()
+        {
+            Correo c = new Correo();
+
+            c += new Paquete("Venezuela 200 ", "345-800-4554");
+            c += new Paquete("Venezuela 200 ", "345-800-4554");
+        }
+
+        /// <summary>
+        /// Verifiqua que la lista de Paquetes del Correo esté instanciada.
+        /// </summary>
+        [TestMethod]
+        public void InstanciaDeListaPaquetes()
+        {
+            Correo c = new Correo();
+            Assert.IsNotNull(c.Paquetes);
+        }
+      
+    }
+}
